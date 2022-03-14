4a54f068db9236fc9e12ef4ad391659beb27bf8c
takumi ikeda <bshuge02@gmail.com>
2022-03-15 02:53:23 +0900

    add:gameObjects

--- game.js
+++ game.js
@@ -1,20 +1,27 @@
 const ctx = document.getElementById('screen').getContext('2d');
+const gameObjects = [{ type: 'player', x: 30, y: 50 }];
+
+const functions = {
+  player: (obj) => {
+    obj.x += 0.2;
+
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.moveTo(obj.x, obj.y);
+    ctx.lineTo(obj.x, obj.y + 20);
+    ctx.stroke();
+  },
+};
 
 function gameLoop() {
-  const begin = Date.now();
-
-  //　画面を消去
-  ctx.clearRect(0, 0, 320, 240);
-
-  // ゲームロジック
-  ctx.lineWidth = 1;
-  ctx.beginPath();
-  ctx.moveTo(10, 10);
-  ctx.lineTo(20, 20);
-  ctx.stroke();
-
+   const begin = Date.now();
+   ctx.clearRect(0, 0, 320, 240); // 画面を消去
+  // ゲームオブジェクトを処理
+  gameObjects.forEach((obj) => {
+    functions[obj.type](obj);
+  });
   const end = Date.now();
-  setTimeout(gameLoop, 33 - (end - begin)); // 0.33msから実際かかった時間を引いた秒数をまつ
+  setTimeout(gameLoop, 33 - (end - begin)); // 0.33msから実際かかった時間を引いた秒数待つ
 }
 
 gameLoop();
