95312bce4453d27bcba34afd23b9287c54b6c339
takumi ikeda <bshuge02@gmail.com>
2022-03-16 03:14:15 +0900

    add:function_keyaction

--- game.js
+++ game.js
@@ -3,7 +3,55 @@ let prevFps = 0;
 let fps = 0;
 let prevTime = Date.now();
 
-const gameObjects = [{ type: 'player', x: 30, y: 50 },{type: 'fps'}];
+const keyCodes = {
+  65: 'left',
+  87: 'top',
+  68: 'right',
+  83: 'bottom',
+  32: 'a',
+  16: 'b',
+};
+
+const keyStatus = {
+  left: false,
+  right: false,
+  top: false,
+  bottom: false,
+  a: false,
+  b: false,
+};
+
+let gamePadIndex;
+
+// ゲームパッドからの入力を保持する
+let gameInput = {
+  left: false,
+  right: false,
+  top: false,
+  bottom: false,
+  a: false,
+  b: false,
+};
+
+window.addEventListener(
+  'keydown',
+  (e) => {
+    if (!(e.key in keyCodes)) return;
+
+    keyStatus[keyCodes[e.key]] = true;
+  },
+  false
+);
+
+window.addEventListener(
+  'keyup',
+  (e) => {
+    if (!(e.keyC in keyCodes)) return;
+
+    keyStatus[keyCodes[e.key]] = false
+  },
+  false
+);
 
 const functions = {
   player: (obj) => {
@@ -18,18 +66,18 @@ const functions = {
   fps: (obj) =>{
     // FPSを画面に描画する
     ctx.fillText('fps: ' + prevFps.toString(), 10, 15);
+  },
+  gameInput: () => {
+    // パッドからの入力を画面に表示する
+    let s = '';
+    s += gameInput.left ? 'L' : '_';
+    s += gameInput.top ? 'T' : '_';
+    s += gameInput.right ? 'R' : '_';
+    s += gameInput.bottom ? 'B' : '_';
+    s += gameInput.a ? 'A' : '_';
+    s += gameInput.b ? 'B' : '_';
+    ctx.fillText('controller: ' + s, 10, 30);
   }
-};
-let gamePadIndex;
-
-// ゲームパッドからの入力を保持する
-let gameInput = {
-  left: false,
-  right: false,
-  top: false,
-  bottom: false,
-  a: false,
-  b: false,
 };
 
 addEventListener('gamepadconnected', (e) => {
@@ -37,20 +85,30 @@ addEventListener('gamepadconnected', (e) => {
   gamePadIndex = e.gamepad.index;
 });
 
+const gameObjects = [{ type: 'player', x: 30, y: 50 },{ type: 'fps'},{ type: 'gameInput'}];
+
 function gameLoop() {
-   const begin = Date.now();
-   ctx.clearRect(0, 0, 320, 240); // 画面を消去
+  const begin = Date.now();
+  ctx.clearRect(0, 0, 320, 240); // 画面を消去
+
+  gameInput = {
+    left: keyStatus.left,
+    right: keyStatus.right,
+    top: keyStatus.top,
+    bottom: keyStatus.bottom,
+    a: keyStatus.a,
+    b: keyStatus.b
+  };
   if (gamePadIndex !== undefined) {
-    // パッドが接続されていれば尿力を取得する
+    // パッドが接続されていればキーボからの入力と結合する
     const gamePad = navigator.getGamepads()[gamePadIndex];
-    gameInput = {
-      left: (gamePad.axes[0] < -0.5),
-      right: (gamePad.axes[0] > 0.5),
-      top: (gamePad.axes[1] < -0.5),
-      bottom: (gamePad.axes[1] > 0.5),
-      a: (gamePad.buttons[1].pressed),
-      b: (gamePad.buttons[0].pressed)
-    };
+
+    gameInput.left |= gamePad.axes[0] < -0.5;
+    gameInput.right |= gamePad.axes[0] < 0.5;
+    gameInput.top |= gamePad.axes[1] < -0.5;
+    gameInput.bottom |= gamePad.axes[1] < 0.5;
+    gameInput.a |= gamePad.buttons[1].pressed;
+    gameInput.b |= gamePad.buttons[0].pressed;
   }
    // ゲームオブジェクトを処理
    gameObjects.forEach((obj) => {
